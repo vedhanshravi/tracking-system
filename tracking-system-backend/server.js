008e1960ec239e6b5009d518ac73987d1da31f82
vedhanshravi <vedhansh.ravi@gmail.com>
2026-02-24 22:27:54 +0530

    Initial commit

---
+++ tracking-system-backend/server.js
@@ -0,0 +1,41 @@
+const pool = require("./config/db");
+const express = require("express");
+const cors = require("cors");
+const bcrypt = require("bcrypt")
+require("dotenv").config();
+ 
+const app = express();
+ 
+app.use(cors());
+app.use(express.json());
+ 
+app.get("/test-db", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Database connection failed");
+  }
+});
+ 
+const userRoutes = require("./routes/userRoutes");
+app.use("/users", userRoutes);
+ 
+
+ 
+const PORT = process.env.PORT || 5000;
+ 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+const authMiddleware = require("./middleware/authMiddleware");
+ 
+app.get("/protected", authMiddleware, (req, res) => {
+  res.json({
+    message: "You accessed protected route",
+    user: req.user,
+  });
+});
+ 
