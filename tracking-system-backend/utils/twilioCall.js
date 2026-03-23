b8ca3c76f5ead2004404972a91494ec4b6b382fc
vedhanshravi <vedhansh.ravi@gmail.com>
2026-03-24 00:53:47 +0530

    Updated Whatsapp config

--- tracking-system-backend/utils/twilioCall.js
+++ tracking-system-backend/utils/twilioCall.js
@@ -5,4 +5,13 @@ const client = twilio(
   process.env.TWILIO_AUTH_TOKEN
 );
 
-module.exports = { client };
+async function sendSmsMessage(toPhone, body) {
+  const message = await client.messages.create({
+    from: process.env.TWILIO_PHONE_NUMBER || "+13502206189",
+    to: toPhone,
+    body,
+  });
+  return message.sid;
+}
+
+module.exports = { client, sendSmsMessage };
